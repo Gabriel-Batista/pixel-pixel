fd2e07711c7d18294ca469794e4f342f089fa9af
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-09 14:01:35 -0500

    Adds image and removes description in ProjectCard

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 
 const ProjectCard = (props) => {
     return (
         <Card raised onClick={props.handleClick}>
+            <Image src={props.base64} />
             <Card.Content>
                 <Card.Header>{props.name}</Card.Header>
                 <Card.Meta>
                     <span>Frames:</span>
                 </Card.Meta>
-                <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+                <Card.Description></Card.Description>
             </Card.Content>
         </Card>
     )
