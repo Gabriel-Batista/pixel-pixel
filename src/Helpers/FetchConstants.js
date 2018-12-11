8d0d46bfb78761d88716eaabe2bf8d1163c642c3
Gabriel-Batista <gabedbatista@gmail.com>
2018-12-11 00:06:00 -0500

    changed fetch URL

--- src/Helpers/FetchConstants.js
+++ src/Helpers/FetchConstants.js
@@ -1,9 +1,4 @@
-
 export const fetchConsts = {
-  API: "https://pixel-pixel-backend.herokuapp.com",
-    
-    HEADERS: {
-        "Content-type": "application/json",
-        "Accepts": "application/json"
-    }
-}
+  API: "https://pixel-pixel-backend.us-east-1.elasticbeanstalk.com",
+  HEADERS: { "Content-type": "application/json", Accepts: "application/json" }
+};
