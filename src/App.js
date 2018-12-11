5258dc26973a4775e29e150a4af34ae8eb3c46c7
Gabriel-Batista <gabedbatista@gmail.com>
2018-12-11 13:32:12 -0500

    Deleting last fram now creates new frame

--- src/App.js
+++ src/App.js
@@ -23,235 +23,219 @@ import AppStyles from "./Styles/AppStyles";
 import { connect } from "react-redux";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        const token = localStorage.getItem("token");
-        if (token) {
-            this.props.userLoggedIn();
+  constructor(props) {
+    super(props);
+    const token = localStorage.getItem("token");
+    if (token) {
+      this.props.userLoggedIn();
 
-            UserFetches.fetchPersistUser(token)
-                .then(res => {
-                    this.props.userLoggedIn(res.username);
-                })
-                .then(res =>
-                    ProjectFetches.fetchProjects().then(res =>
-                        this.props.pullProjects(res)
-                    )
-                );
-        }
-        this.state = {
-            titleEdit: false
-        };
-    }
-
-    toggleTitleEdit = () => {
-        this.setState({
-            titleEdit: !this.state.titleEdit
-        });
-    };
-
-    render() {
-        return (
-            <Grid
-                style={{ marginTop: "auto",}}
-                centered
-                columns={16}
-            >
-            <Grid.Row centered compact columns={16}>
-                    {/* LEFT COLUMN */}
-              <Grid.Column width={3} style={{ backgroundColor: "#1F1F1F"}}>
-                        {/* LOGO */}
-                        <Segment style={AppStyles.SegmentBGColor}>
-                            <Header
-                                as="h1"
-                                textAlign="center"
-                                style={{ marginTop: "10px", color: "#FFFFFF" }}
-                            >
-                                <Header.Content>--PIXEL-PIXEL--</Header.Content>
-                            </Header>
-                        </Segment>
-                        <Segment style={{...AppStyles.SegmentBGColor, ...AppStyles.ToolBoxSegmentStyles }}>
-                            {/* TOOLBOX */}
-                            <Header
-                                textAlign="center"
-                                size="huge"
-                                style={{ marginTop: "30px" }}
-                            >
-                                <Header.Content style={{ color: "#FFFFFF" }}>
-                                    TOOLBOX
-                                </Header.Content>
-                            </Header>
-                            <ToolBox />
-                        </Segment>
-                    </Grid.Column>
-                    {/* CENTER COLUMN */}
-                    <Grid.Column columns={10} width={6}>
-                        {/* NAME INPUT */}
-                        <Segment style={AppStyles.SegmentBGColor}>
-                            {this.state.titleEdit ? (
-                                <Input
-                                    autoFocus
-                                    value={this.props.projectName}
-                                    onChange={e =>
-                                        this.props.changeProjectName(
-                                            e.target.value
-                                        )
-                                    }
-                                    onBlur={() => {
-                                        this.toggleTitleEdit();
-                                        if (this.props.projectId !== null) {
-                                            ProjectFetches.fetchUpdateProject({
-                                                projectId: this.props.projectId,
-                                                frameId: this.props
-                                                    .selectedCanvas,
-                                                token: localStorage.getItem(
-                                                    "token"
-                                                ),
-                                                name: this.props.projectName,
-                                                frame: this.props.canvasRef.current.toDataURL()
-                                            });
-                                        }
-                                    }}
-                                    size="massive"
-                                    label="Title:"
-                                    style={{ width: "100%" }}
-                                />
-                            ) : (
-                                <Header
-                                    as="h1"
-                                    size="huge"
-                                    textAlign="center"
-                                    onClick={this.toggleTitleEdit}
-                                    style={{
-                                        color: "#FFFFFF",
-                                        margin: "15px 0px"
-                                    }}
-                                >
-                                    {this.props.projectName}
-                                </Header>
-                            )}
-                        </Segment>
-                        {/* CANVAS */}
-                        <Segment style={{ backgroundColor: "#515151" }} textAlign="center">
-                            <Canvas/>
-                        </Segment>
-                        {/* FRAMES */}
-                        <p style={AppStyles.SegmentBGColor}>
-                            <Header
-                                textAlign="left"
-                                size="large"
-                                style={{
-                                    marginTop: "10px",
-                                    marginLeft: "25px",
-                                    color: "#FFFFFF"
-                                }}
-                            >
-                                <Header.Content>FRAMES</Header.Content>
-                            </Header>
-                            <Frame />
-                        </p>
-                    </Grid.Column>
-                    {/* RIGHT COLUMN */}
-                    <Grid.Column width={3}>
-                        {/* GIF */}
-                        <Segment
-                            textAlign="center"
-                            style={AppStyles.SegmentBGColor}
-                        >
-                            <Header textAlign="center" size="large">
-                                <Header.Content style={{ color: "#FFFFFF" }}>
-                                    GIF
-                                </Header.Content>
-                            </Header>
-                            <Gif />
-                        </Segment>
-                        {/* PREVIEW */}
-                        <Segment
-                            textAlign="center"
-                            style={AppStyles.SegmentBGColor}
-                        >
-                            <Header textAlign="center" size="large">
-                                <Header.Content style={{ color: "#FFFFFF" }}>
-                                    PREVIEW
-                                </Header.Content>
-                            </Header>
-                            {this.props.canvasRef ? (
-                                <Preview
-                                    border
-                                    canvasToRender={
-                                        this.props.canvasRef.current
-                                    }
-                                    history={this.props.history}
-                                />
-                            ) : null}
-                        </Segment>
-                        <Segment style={AppStyles.SegmentBGColor}>
-                            <Header
-                                textAlign="center"
-                                size="huge"
-                                style={{
-                                    marginTop: "20px",
-                                    marginBottom: "40px",
-                                    color: "#FFFFFF"
-                                }}
-                            >
-                                <Header.Content>MENU</Header.Content>
-                            </Header>
-                            <NewButton />
-                            {this.props.status === "logged in" ? (
-                                <SaveButton />
-                            ) : null}
-                            {this.props.status === "logged in" ? (
-                                <Projects />
-                            ) : null}
-                            <GridButton />
-                            {this.props.status === "logged out" ? (
-                                <Login />
-                            ) : (
-                                <Logout />
-                            )}
-                        </Segment>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+      UserFetches.fetchPersistUser(token)
+        .then(res => {
+          this.props.userLoggedIn(res.username);
+        })
+        .then(res =>
+          ProjectFetches.fetchProjects().then(res =>
+            this.props.pullProjects(res)
+          )
         );
     }
+    this.state = {
+      titleEdit: false
+    };
+  }
+
+  toggleTitleEdit = () => {
+    this.setState({
+      titleEdit: !this.state.titleEdit
+    });
+  };
+
+  render() {
+    return (
+      <Grid style={{ marginTop: "auto" }} centered columns={16}>
+        <Grid.Row centered compact columns={16}>
+          {/* LEFT COLUMN */}
+          <Grid.Column width={3} style={{ backgroundColor: "#1F1F1F" }}>
+            {/* LOGO */}
+            <Segment style={AppStyles.SegmentBGColor}>
+              <Header
+                as="h1"
+                textAlign="center"
+                style={{ marginTop: "10px", color: "#FFFFFF" }}
+              >
+                <Header.Content>--PIXEL-PIXEL--</Header.Content>
+              </Header>
+            </Segment>
+            <Segment
+              style={{
+                ...AppStyles.SegmentBGColor,
+                ...AppStyles.ToolBoxSegmentStyles
+              }}
+            >
+              {/* TOOLBOX */}
+              <Header
+                textAlign="center"
+                size="huge"
+                style={{ marginTop: "30px" }}
+              >
+                <Header.Content style={{ color: "#FFFFFF" }}>
+                  TOOLBOX
+                </Header.Content>
+              </Header>
+              <ToolBox />
+            </Segment>
+          </Grid.Column>
+          {/* CENTER COLUMN */}
+          <Grid.Column columns={10} width={6}>
+            {/* NAME INPUT */}
+            <Segment style={AppStyles.SegmentBGColor}>
+              {this.state.titleEdit ? (
+                <Input
+                  autoFocus
+                  value={this.props.projectName}
+                  onChange={e => this.props.changeProjectName(e.target.value)}
+                  onBlur={() => {
+                    this.toggleTitleEdit();
+                    if (this.props.projectId !== null) {
+                      ProjectFetches.fetchUpdateProject({
+                        projectId: this.props.projectId,
+                        frameId: this.props.selectedCanvas,
+                        token: localStorage.getItem("token"),
+                        name: this.props.projectName,
+                        frame: this.props.canvasRef.current.toDataURL()
+                      });
+                    }
+                  }}
+                  size="massive"
+                  label="Title:"
+                  style={{ width: "100%" }}
+                />
+              ) : (
+                <Header
+                  as="h1"
+                  size="huge"
+                  textAlign="center"
+                  onClick={this.toggleTitleEdit}
+                  style={{
+                    color: "#FFFFFF",
+                    margin: "15px 0px"
+                  }}
+                >
+                  {this.props.projectName}
+                </Header>
+              )}
+            </Segment>
+            {/* CANVAS */}
+            <Segment style={{ backgroundColor: "#515151" }} textAlign="center">
+              <Canvas />
+            </Segment>
+            {/* FRAMES */}
+            <div style={AppStyles.SegmentBGColor}>
+              <Header
+                textAlign="left"
+                size="large"
+                style={{
+                  marginTop: "25px",
+                  marginLeft: "25px",
+                  color: "#FFFFFF"
+                }}
+              >
+                <Header.Content
+                  style={{
+                    marginTop: "25px"
+                  }}
+                >
+                  FRAMES
+                </Header.Content>
+              </Header>
+              <Frame />
+            </div>
+          </Grid.Column>
+          {/* RIGHT COLUMN */}
+          <Grid.Column width={3}>
+            {/* GIF */}
+            <Segment textAlign="center" style={AppStyles.SegmentBGColor}>
+              <Header textAlign="center" size="large">
+                <Header.Content style={{ color: "#FFFFFF" }}>
+                  GIF
+                </Header.Content>
+              </Header>
+              <Gif />
+            </Segment>
+            {/* PREVIEW */}
+            <Segment textAlign="center" style={AppStyles.SegmentBGColor}>
+              <Header textAlign="center" size="large">
+                <Header.Content style={{ color: "#FFFFFF" }}>
+                  PREVIEW
+                </Header.Content>
+              </Header>
+              {this.props.canvasRef ? (
+                <Preview
+                  border
+                  canvasToRender={this.props.canvasRef.current}
+                  history={this.props.history}
+                />
+              ) : null}
+            </Segment>
+            <Segment style={AppStyles.SegmentBGColor}>
+              <Header
+                textAlign="center"
+                size="huge"
+                style={{
+                  marginTop: "20px",
+                  marginBottom: "40px",
+                  color: "#FFFFFF"
+                }}
+              >
+                <Header.Content>MENU</Header.Content>
+              </Header>
+              <NewButton />
+              {this.props.status === "logged in" ? <SaveButton /> : null}
+              {this.props.status === "logged in" ? <Projects /> : null}
+              <GridButton />
+              {this.props.status === "logged out" ? <Login /> : <Logout />}
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        canvasRef: state.canvas.canvasRef,
-        history: state.history.selectedHistory,
-        selectedFrame: state.canvas.frameId,
-        status: state.users.status,
-        projectName: state.projects.projectName
-    };
+  return {
+    canvasRef: state.canvas.canvasRef,
+    history: state.history.selectedHistory,
+    selectedFrame: state.canvas.frameId,
+    status: state.users.status,
+    projectName: state.projects.projectName
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        changeProjectName: payload => {
-            dispatch({
-                type: "CHANGE_PROJECT_NAME",
-                payload: payload
-            });
-        },
-        pullProjects: payload => {
-            dispatch({
-                type: "PULL_PROJECTS",
-                payload: payload
-            });
-        },
-        userLoggedIn: payload => {
-            dispatch({
-                type: "USER_LOGGED_IN",
-                payload: payload
-            });
-        }
-    };
+  return {
+    changeProjectName: payload => {
+      dispatch({
+        type: "CHANGE_PROJECT_NAME",
+        payload: payload
+      });
+    },
+    pullProjects: payload => {
+      dispatch({
+        type: "PULL_PROJECTS",
+        payload: payload
+      });
+    },
+    userLoggedIn: payload => {
+      dispatch({
+        type: "USER_LOGGED_IN",
+        payload: payload
+      });
+    }
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(App);
--- src/Components/Frame.js
+++ src/Components/Frame.js
@@ -1,146 +1,174 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Button, Icon } from 'semantic-ui-react';
-import UUID from 'uuid/v4'
-import { ProjectFetches } from '../Helpers/ProjectAdapter'
-import FrameStyles from '../Styles/FrameStyles'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button, Icon } from "semantic-ui-react";
+import UUID from "uuid/v4";
+import { ProjectFetches } from "../Helpers/ProjectAdapter";
+import FrameStyles from "../Styles/FrameStyles";
 
-import FrameCard from './FrameCard'
+import FrameCard from "./FrameCard";
 
-class Frame extends Component   {
-    
-    componentDidUpdate= () =>   {
-        this.drawGhost()
+class Frame extends Component {
+  componentDidUpdate = () => {
+    this.drawGhost();
+  };
+
+  newFrame = () => {
+    const id = UUID();
+    this.props.newFrame(id);
+    // this.props.updateFrame({ id: this.props.frameId, base64: this.props.canvasRef.current.toDataURL()})
+    this.props.context.clearRect(
+      0,
+      0,
+      this.props.canvasWidth,
+      this.props.canvasHeight
+    );
+    this.props.selectFrame(id);
+    if (this.props.projectId !== null) {
+      ProjectFetches.fetchCreateFrame({
+        project_id: this.props.projectId,
+        frame_id: id,
+        base64: ""
+      });
     }
+  };
 
-    newFrame = () => {
-        const id = UUID()
-        this.props.newFrame(id)
-        // this.props.updateFrame({ id: this.props.frameId, base64: this.props.canvasRef.current.toDataURL()})
-        this.props.context.clearRect(0, 0, this.props.canvasWidth, this.props.canvasHeight)
-        this.props.selectFrame(id)
-        if (this.props.projectId !== null) {
-            ProjectFetches.fetchCreateFrame({project_id: this.props.projectId, frame_id: id, base64: ""})
-        }
+  deleteFrame = id => {
+    ProjectFetches.fetchDeleteFrame(id).then(res => {
+      this.props.deleteFrame(id);
+      if (Object.keys(this.props.frames).length === 0) {
+        this.newFrame();
+      }
+    });
+
+    this.bringCanvasToFront(Object.values(this.props.frames)[0].id);
+  };
+
+  drawGhost = () => {
+    this.props.ghostContext.clearRect(
+      0,
+      0,
+      this.props.canvasWidth,
+      this.props.canvasHeight
+    );
+    let frameIndex = Object.keys(this.props.frames).indexOf(this.props.frameId);
+    if (Object.keys(this.props.frames).length > 0 && frameIndex > 0) {
+      let ghostImg = new Image();
+      ghostImg.src = Object.values(this.props.frames)[frameIndex - 1].base64;
+      ghostImg.onload = () => {
+        this.props.ghostContext.globalAlpha = 0.4;
+        this.props.ghostContext.drawImage(ghostImg, 0, 0);
+      };
     }
+  };
 
-    deleteFrame = (id) => {
-        ProjectFetches.fetchDeleteFrame(id)
-        .then(res => this.props.deleteFrame(id))
-        this.bringCanvasToFront(Object.values(this.props.frames)[0].id)
-    }
+  bringCanvasToFront = id => {
+    this.props.selectFrame(id);
 
-    drawGhost = () =>   {
-        this.props.ghostContext.clearRect(0, 0, this.props.canvasWidth, this.props.canvasHeight)
-        let frameIndex = Object.keys(this.props.frames).indexOf(this.props.frameId)
-        if (Object.keys(this.props.frames).length > 0 && frameIndex > 0) {
-            let ghostImg = new Image()
-            ghostImg.src = Object.values(this.props.frames)[frameIndex - 1].base64
-            ghostImg.onload = () => {
-                this.props.ghostContext.globalAlpha = 0.4
-                this.props.ghostContext.drawImage(ghostImg, 0, 0)
-            }
-        }
-    }
+    let tmpImg = new Image();
+    tmpImg.src = this.props.frames[id].base64;
 
-    bringCanvasToFront = (id) => {
-        this.props.selectFrame(id)
+    this.props.context.clearRect(
+      0,
+      0,
+      this.props.canvasWidth,
+      this.props.canvasHeight
+    );
+    tmpImg.onload = () => {
+      this.props.context.drawImage(tmpImg, 0, 0);
 
-        let tmpImg = new Image()
-        tmpImg.src = this.props.frames[id].base64
+      this.props.previewContext.clearRect(
+        0,
+        0,
+        this.props.canvasWidth,
+        this.props.canvasHeight
+      );
+      this.props.previewContext.drawImage(
+        tmpImg,
+        0,
+        0,
+        this.props.canvasWidth / 3,
+        this.props.canvasHeight / 3
+      );
+    };
+  };
 
-        this.props.context.clearRect(0, 0, this.props.canvasWidth, this.props.canvasHeight)
-        tmpImg.onload = () => {
-            this.props.context.drawImage(tmpImg, 0, 0)
+  renderFrames = () => {
+    return Object.values(this.props.frames).map(frame => {
+      let tmpImg = new Image();
+      tmpImg.src = frame.base64;
+      return (
+        <FrameCard
+          frame={frame}
+          tmpImg={tmpImg}
+          deleteFrame={this.deleteFrame}
+          bringCanvasToFront={this.bringCanvasToFront}
+        />
+      );
+    });
+  };
 
-            this.props.previewContext.clearRect(0, 0, this.props.canvasWidth, this.props.canvasHeight)
-            this.props.previewContext.drawImage(tmpImg, 0, 0, (this.props.canvasWidth / 3), ((this.props.canvasHeight) / 3))
-        }
-    }
-
-    renderFrames= () => {
-        return Object.values(this.props.frames).map((frame) => {
-            let tmpImg = new Image()
-            tmpImg.src = frame.base64
-            return (
-                <FrameCard 
-                    frame={frame} 
-                    tmpImg={tmpImg} 
-                    deleteFrame={this.deleteFrame} 
-                    bringCanvasToFront={this.bringCanvasToFront}
-                ></FrameCard>
-            )
-        })
-    }
-
-    render()    {
-        return  (
-            <div style={FrameStyles.FramesContainer}>
-                {this.renderFrames()}
-                <Button 
-                    icon
-                    onClick={this.newFrame}
-                    style={FrameStyles.newFrameButton}
-                >
-                    <Icon 
-                        name="add" 
-                        size="huge"
-                        style={{verticalAlign:"middle"}}
-                    ></Icon>
-                </Button>
-            </div>
-            
-        )
-    }
+  render() {
+    return (
+      <div style={FrameStyles.FramesContainer}>
+        {this.renderFrames()}
+        <Button icon onClick={this.newFrame} style={FrameStyles.newFrameButton}>
+          <Icon name="add" size="huge" style={{ verticalAlign: "middle" }} />
+        </Button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps= (state) =>   {
-    return {
-        frames: state.history.frames,
-        canvasRef: state.canvas.canvasRef,
-        context: state.canvas.context,
-        ghostContext: state.canvas.ghostContext,
-        canvasWidth: state.canvas.width,
-        canvasHeight: state.canvas.height,
-        previewContext: state.canvas.previewContext,
-        frameId: state.canvas.frameId,
-        projectId: state.projects.projectId
-    }
-}
+const mapStateToProps = state => {
+  return {
+    frames: state.history.frames,
+    canvasRef: state.canvas.canvasRef,
+    context: state.canvas.context,
+    ghostContext: state.canvas.ghostContext,
+    canvasWidth: state.canvas.width,
+    canvasHeight: state.canvas.height,
+    previewContext: state.canvas.previewContext,
+    frameId: state.canvas.frameId,
+    projectId: state.projects.projectId
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        selectFrame: (payload) => {
-            dispatch({
-                type: 'SET_FRAME_ID',
-                payload: payload
-            })
-        },
-        pushFrame: (payload) => {
-            dispatch({
-                type: 'PUSH_FRAME',
-                payload: payload
-            })
-        },
-        newFrame: (payload) => {
-            dispatch({
-                type: "NEW_FRAME",
-                payload: payload
-            })
-        },
-        updateFrame: (payload) => {
-            dispatch({
-                type: "UPDATE_FRAME",
-                payload: payload
-            })
-        },
-        deleteFrame: (payload) => {
-            dispatch({
-                type: "DELETE_FRAME",
-                payload: payload
-            })
-        }
+const mapDispatchToProps = dispatch => {
+  return {
+    selectFrame: payload => {
+      dispatch({
+        type: "SET_FRAME_ID",
+        payload: payload
+      });
+    },
+    pushFrame: payload => {
+      dispatch({
+        type: "PUSH_FRAME",
+        payload: payload
+      });
+    },
+    newFrame: payload => {
+      dispatch({
+        type: "NEW_FRAME",
+        payload: payload
+      });
+    },
+    updateFrame: payload => {
+      dispatch({
+        type: "UPDATE_FRAME",
+        payload: payload
+      });
+    },
+    deleteFrame: payload => {
+      dispatch({
+        type: "DELETE_FRAME",
+        payload: payload
+      });
     }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Frame)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Frame);
