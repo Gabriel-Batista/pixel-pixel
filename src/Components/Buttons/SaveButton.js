b271239d01fb48a815d75df18bfd2c7c80130c36
Gabriel-Batista <gabedbatista@gmail.com>
2018-12-12 17:50:13 -0500

    Adds pop up to notify user project has been saved

--- src/Components/Buttons/SaveButton.js
+++ src/Components/Buttons/SaveButton.js
@@ -1,46 +1,71 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
-import { ProjectFetches } from '../../Helpers/ProjectAdapter'
-import { connect } from 'react-redux';
+import React from "react";
+import { Button, Modal, Header, Icon } from "semantic-ui-react";
+import { ProjectFetches } from "../../Helpers/ProjectAdapter";
+import { connect } from "react-redux";
 
-const SaveButton = (props) =>   {
-    const saveFrame = () => {
-        ProjectFetches.fetchCreateProject({
-            token: localStorage.getItem('token'),
-            name: props.projectName,
-            frames: props.frames
-        })
+const SaveButton = props => {
+  const saveFrame = () => {
+    ProjectFetches.fetchCreateProject({
+      token: localStorage.getItem("token"),
+      name: props.projectName,
+      frames: props.frames
+    });
 
-        ProjectFetches.fetchProjects()
-            .then(res => props.pullProjects(res))
-    }
+    ProjectFetches.fetchProjects().then(res => props.pullProjects(res));
+  };
 
-    return (
+  return (
+    <Modal
+      trigger={
         <Button
-            fluid
-            onClick={() => { saveFrame() }}
-            size="massive"
-            style={{ marginBottom: "25px", backgroundColor: "#515151", color: "#FFFFFF"}}
-        >SAVE</Button>
-    )
-}
+          fluid
+          onClick={() => {
+            saveFrame();
+          }}
+          size="massive"
+          style={{
+            marginBottom: "25px",
+            backgroundColor: "#515151",
+            color: "#FFFFFF"
+          }}
+        >
+          SAVE
+        </Button>
+      }
+      basic
+      size="small"
+    >
+      <Header icon="archive" content="Project Was Saved!" />
+      <Modal.Content>
+        <p>
+          Your project has been save!
+          Click on 'Projects' to view it!
+        </p>
+      </Modal.Content>
+      
+    </Modal>
+  );
+};
 
-const mapStateToProps = (state) =>  {
-    return {
-        projectName: state.projects.projectName,
-        frames: state.history.frames
+const mapStateToProps = state => {
+  return {
+    projectName: state.projects.projectName,
+    frames: state.history.frames
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    pullProjects: payload => {
+      dispatch({
+        type: "PULL_PROJECTS",
+        payload: payload
+      });
     }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) =>    {
-    return  {
-        pullProjects: (payload) => {
-            dispatch({
-                type: 'PULL_PROJECTS',
-                payload: payload
-            })
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SaveButton)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SaveButton);
